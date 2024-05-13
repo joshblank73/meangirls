88d989f74e10daaf2b6e1b4affd197f3e5df5a8e
Joshua Blank <jwblank@calpoly.edu>
2024-05-13 11:11:07 -0700

    Fixed unit test failures

--- R/give_candygrams.R
+++ R/give_candygrams.R
@@ -6,7 +6,7 @@
 #'
 #' @return A candy gram announcement
 #'
-#' @importFrom stringr str_detect str_to_title
+#' @importFrom stringr str_detect str_to_title str_trim
 #' @importFrom english as.english
 #'
 #' @export
@@ -30,7 +30,7 @@ give_candygrams <- function(person, number,
   number <- str_to_title(as.english(number))
 
 
-  glue::glue("{number} for {person}. {extra_message}")
+  stringr::str_trim(glue::glue("{number} for {person}. {extra_message}"))
 
 
 
